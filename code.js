c70efb8369925afc305008d3fb8240a013421657
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-08-07 19:14:15 -0400

    Functions for making and deleting star

--- code.js
+++ code.js
@@ -1,25 +1,36 @@
 var svg = document.querySelector("svg");
 svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
 
-var star = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
 
-var numpts = 42;
-var points = "";
+function makeStar() {
+    var star = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
 
-for (var i = 0; i < numpts; i++) {
-    var spokeLength = Math.random() * 2 + 1;
-    points += `${
-        960 + Math.cos(i * 2 * Math.PI / numpts) * 300 * spokeLength
-    },${
-        960 + Math.sin(i * 2 * Math.PI / numpts) * 300 * spokeLength
-    } `;
-    points += `${
-        960 + Math.cos((i * 2 + 1) * Math.PI / numpts) * 30
-    },${
-        960 + Math.sin((i * 2 + 1) * Math.PI / numpts) * 30
-    } `;
+    var numpts = 42;
+    var points = "";
+
+    for (var i = 0; i < numpts; i++) {
+        var spokeLength = Math.random() * 2 + 1;
+        points += `${
+            960 + Math.cos(i * 2 * Math.PI / numpts) * 300 * spokeLength
+        },${
+            960 + Math.sin(i * 2 * Math.PI / numpts) * 300 * spokeLength
+        } `;
+        points += `${
+            960 + Math.cos((i * 2 + 1) * Math.PI / numpts) * 30
+        },${
+            960 + Math.sin((i * 2 + 1) * Math.PI / numpts) * 30
+        } `;
+    }
+
+    star.setAttribute("points", points);
+    svg.appendChild(star);
+    star.style.fill = "white";
 }
 
-star.setAttribute("points", points);
-svg.appendChild(star);
-star.style.fill = "white";
+function deleteStar() {
+    if (document.querySelectorAll("polygon").length) {
+        document.querySelector("polygon").remove();
+    }
+}
+
+makeStar();
