ba52f0f01793e0bdd04d8100b9342a6b8ef083d4
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-01-16 16:09:21 -0500

    Radius instead of size

--- SpotComponent.js
+++ SpotComponent.js
@@ -4,18 +4,18 @@ class SpotComponent {
         intensity: 10,
         deformationFrequency: 0.006
     };
-    size = 1024;
+    radius = 1024;
 
-    constructor(size, options) {
-        this.size = size;
+    constructor(radius, options) {
+        this.radius = radius;
         for (var opt in options) {
             this.options[opt] = options[opt];
         }
     }
 
     render() {
-        this.canvas.width = this.size;
-        this.canvas.height = this.size;
+        this.canvas.width = this.radius * 2;
+        this.canvas.height = this.radius * 2;
         var ctx = this.canvas.getContext("2d");
     }
 }
