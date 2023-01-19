4367115a22f86569e0b186ae743264ce7cba1fd5
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2023-01-18 20:45:58 -0500

    Save a little more time
    Wow

--- polarCoordinatesFilter.js
+++ polarCoordinatesFilter.js
@@ -5,21 +5,22 @@ function polarCoordinatesFilter(ctx) {
     var inputData = inputImageData.data;
     var outputData = outputImageData.data;
     var origin = { x: canvas.width / 2, y: canvas.height / 2 };
+    var outputX = -1, outputY = 0;
+    var inputX = -1, inputY = 0;
     for (var i = 0; i < outputData.length; i += 4) {
-        var outputCoords = {
-            x: (i / 4) % canvas.width,
-            y: Math.floor((i / 4) / canvas.width)
-        };
-        var angle = Math.atan2(outputCoords.y - origin.y, outputCoords.x - origin.x);
-        var radius = Math.sqrt(Math.pow(outputCoords.x - origin.x, 2) + Math.pow(outputCoords.y - origin.y, 2));
+        outputX++;
+        if (outputX >= canvas.width) {
+            outputY++;
+            outputX = 0;
+        }
+        var angle = Math.atan2(outputY - origin.y, outputX - origin.x);
+        var radius = Math.sqrt(Math.pow(outputX - origin.x, 2) + Math.pow(outputY - origin.y, 2));
         if (radius > canvas.width / 2) {
             continue;
         }
-        var inputCoords = {
-            x: Math.round(angle / Math.PI / 2 * canvas.width),
-            y: Math.round(radius * 2)
-        };
-        var inputI = 4 * (inputCoords.y * canvas.width + inputCoords.x);
+        inputX = Math.round(angle / Math.PI / 2 * canvas.width);
+        inputY = Math.round(radius * 2);
+        var inputI = 4 * (inputY * canvas.width + inputX);
         outputData[i] = inputData[inputI];
         outputData[i + 1] = inputData[inputI + 1];
         outputData[i + 2] = inputData[inputI + 2];
